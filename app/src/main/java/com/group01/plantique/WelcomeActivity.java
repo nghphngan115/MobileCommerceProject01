31e4413efaff92f4f8d861e46c01da3767e01f05
lechanh27102003 <anhlc21411ca@st.uel.edu.vn>
2024-04-07 18:02:50 +0700

    login sign up layout and anim 2

---
+++ app/src/main/java/com/group01/plantique/WelcomeActivity.java
@@ -0,0 +1,52 @@
+package com.group01.plantique;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        TextView signInTextView = findViewById(R.id.txtSignIn);
+        TextView signUpTextView = findViewById(R.id.txtSignUp);
+
+        // Set click listener for Sign in TextView
+        signInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Apply animation
+                animateView(v);
+
+                // Start LoginActivity
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set click listener for Sign up TextView
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Apply animation
+                animateView(v);
+
+                // Start SignUpActivity
+                Intent intent = new Intent(WelcomeActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void animateView(View view) {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.button_bounce);
+        view.startAnimation(anim);
+    }
+}
