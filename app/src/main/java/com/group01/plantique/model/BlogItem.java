543314fee885d63c174d8d806fe8a57fb03499b5
nguyenbaoannhien6642 <nhiennba21411ca@st.uel.edu.vn>
2024-04-25 23:18:10 +0700

    Merge remote-tracking branch 'origin/An_Nhien_blog_cart_new_version_15_04' into An_Nhien_blog_cart_new_version_15_04

--- app/src/main/java/com/group01/plantique/model/BlogItem.java
+++ app/src/main/java/com/group01/plantique/model/BlogItem.java
@@ -1,31 +1,53 @@
 package com.group01.plantique.model;
 
 public class BlogItem {
-    private String blogId; // Thêm trường blogId
+    private String blogId;
     private String blogTitle;
     private String blogImage;
     private String blogContent;
 
-    public BlogItem(String blogId, String title, String image, String content) {
-        this.blogId = blogId;
-        this.blogTitle = title;
-        this.blogImage = image;
-        this.blogContent = content;
+    // Constructor
+    public BlogItem() {
+        // Default constructor required for calls to DataSnapshot.getValue(BlogItem.class)
     }
 
+    public BlogItem(String blogId, String blogTitle, String blogImage, String blogContent) {
+        this.blogId = blogId;
+        this.blogTitle = blogTitle;
+        this.blogImage = blogImage;
+        this.blogContent = blogContent;
+    }
+
+    // Getters and setters
     public String getBlogId() {
         return blogId;
+    }
+
+    public void setBlogId(String blogId) {
+        this.blogId = blogId;
     }
 
     public String getTitle() {
         return blogTitle;
     }
 
+    public void setTitle(String blogTitle) {
+        this.blogTitle = blogTitle;
+    }
+
     public String getImage() {
         return blogImage;
     }
 
+    public void setImage(String blogImage) {
+        this.blogImage = blogImage;
+    }
+
     public String getContent() {
         return blogContent;
+    }
+
+    public void setContent(String blogContent) {
+        this.blogContent = blogContent;
     }
 }
