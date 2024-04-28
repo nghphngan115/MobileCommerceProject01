dff4fea114366d29157103684356ed001ff18c06
ANHTHU <thupta21411ca@st.uel.edu.vn>
2024-04-28 10:53:37 +0700

    chanh ơi xem lại mấy chỗ kết nối firebase giúp t với, t càng sửa nó càng um sùm

--- app/src/main/java/com/group01/plantique/java/ProductDetailActivity.java
+++ app/src/main/java/com/group01/plantique/java/ProductDetailActivity.java
@@ -3,18 +3,28 @@ package com.group01.plantique.java;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.group01.plantique.R;
+import com.group01.plantique.adapter.AdapterReview;
+import com.group01.plantique.model.ModelReview;
 import com.group01.plantique.model.Product;
 import com.squareup.picasso.Picasso;
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.ArrayList;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
@@ -25,6 +35,15 @@ public class ProductDetailActivity extends AppCompatActivity {
     private TextView textViewProductDiscountPrice;
 
     private DatabaseReference productsRef;
+    private RatingBar ratingBar; //ANH THU
+    private ArrayList<ModelReview> reviewArrayList; //ANH THU, contain list of all reviews
+    private AdapterReview adapterReview; //ANH THU
+    private FirebaseAuth firebaseAuth; //ANH THU
+    private RecyclerView reviewRv; //ANH THU
+    private TextView ratingsTv; //ANH THU
+
+
+
 
 
     @Override
@@ -38,8 +57,59 @@ public class ProductDetailActivity extends AppCompatActivity {
         textViewProductPrice = findViewById(R.id.txtPrice);
         textViewProductDiscountPrice = findViewById(R.id.txtDiscountPrice);
 
+        ratingBar = findViewById(R.id.ratingBar); //ANH THU
+
         String productId = getIntent().getStringExtra("productId");
         productsRef = FirebaseDatabase.getInstance().getReference().child("products").child(productId);
+
+
+        //ANH THU THEM KHUC NAY
+
+        reviewRv = findViewById(R.id.reviewRv);
+        ratingsTv = findViewById(R.id.txtNumberOfReviews);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        loadReview(); //for review list
+    }
+        private float ratingSum = 0;
+        private void loadReview() {
+            //init list
+            reviewArrayList = new ArrayList<>();
+            DatabaseReference ref =FirebaseDatabase.getInstance().getReference("Users");
+            ref.child("Ratings")
+                    .addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //clear list before adding data to it
+                            reviewArrayList.clear();
+                            ratingSum = 0;
+                            for (DataSnapshot ds : snapshot.getChildren()) {
+                                float rating = Float.parseFloat("" + ds.child("ratings").getValue());
+                                ratingSum = ratingSum +rating;
+
+                                ModelReview modelReview = ds.getValue(ModelReview.class);
+                                reviewArrayList.add(modelReview);
+                            }
+                            //set up adapter
+                            adapterReview = new AdapterReview(ProductDetailActivity.this, reviewArrayList);
+                            //set to recyclerview
+                            reviewRv.setAdapter(adapterReview);
+
+                            long numberOfReviews = snapshot.getChildrenCount();
+                            float avgRating= ratingSum/numberOfReviews;
+
+                            ratingsTv.setText(String.format("%.2f", avgRating) + "[" +numberOfReviews+ "]");
+                            ratingBar.setRating(avgRating);
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+            // ANH THU DONE
+
+
+
 
         productsRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
