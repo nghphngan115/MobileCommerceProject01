18bc32d13724e1536e888e7412c35f1507631985
soihoangcodoc123 <trampq21411ca@st.uel.edu.vn>
2024-05-10 22:36:54 +0700

    xong home lần 2

--- app/src/main/java/com/group01/plantique/java/BlogDetailActivity.java
+++ app/src/main/java/com/group01/plantique/java/BlogDetailActivity.java
@@ -6,6 +6,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,6 +32,15 @@ public class BlogDetailActivity extends AppCompatActivity {
         imgBlog = findViewById(R.id.imgBlog);
         imgButton2 = findViewById(R.id.imgButton2);
 
+        // Get data from intent
+        String blogTitle = getIntent().getStringExtra("blogTitle");
+        String blogImage = getIntent().getStringExtra("blogImage");
+        String blogContent = getIntent().getStringExtra("blogContent");
+
+        // Display data
+        txtTitle.setText(blogTitle);
+        txtContent.setText(blogContent);
+        Glide.with(this).load(blogImage).into(imgBlog);
         // Get blog ID from intent
         String blogId = getIntent().getStringExtra("blogId");
 
