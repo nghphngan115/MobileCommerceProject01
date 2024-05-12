ca20dcb9385491485500e3958756ccf79a1cd0b6
macos <ngannhp21411ca@st.uel.edu.vn>
2024-05-12 21:08:07 +0700

    finished

--- app/src/main/java/com/group01/plantique/java/BlogCategoryActivity.java
+++ app/src/main/java/com/group01/plantique/java/BlogCategoryActivity.java
@@ -2,13 +2,10 @@ package com.group01.plantique.java;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,67 +18,74 @@ import com.group01.plantique.R;
 import com.group01.plantique.adapter.BlogAdapter;
 import com.group01.plantique.model.BlogItem;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
-public class BlogCategoryActivity extends AppCompatActivity {
+public class BlogCategoryActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private RecyclerView rvBlog;
     private BlogAdapter adapter;
     private ArrayList<BlogItem> blogItems;
+    private ArrayList<BlogItem> filteredBlogItems;
     private DatabaseReference mDatabase;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_category);
 
-        //Navigation
+        setupNavigationView();
+        initializeFirebaseAndRecyclerView();
+        setupSearchView();
+    }
+
+    private void setupNavigationView() {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.blog);
-
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.blog) {
                 return true;
             } else if (itemId == R.id.home) {
-                startActivity(new Intent(getApplicationContext(), HomeScreenActivity.class));
-                finish();
+                switchActivity(HomeScreenActivity.class);
                 return true;
             } else if (itemId == R.id.cart) {
-                startActivity(new Intent(getApplicationContext(), CartActivity.class));
-                finish();
+                switchActivity(CartActivity.class);
                 return true;
             } else if (itemId == R.id.notification) {
-                startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
-                finish();
+                switchActivity(NotificationActivity.class);
                 return true;
             } else if (itemId == R.id.account) {
-                startActivity(new Intent(getApplicationContext(), UserInformationActivity.class));
-                finish();
+                switchActivity(UserInformationActivity.class);
                 return true;
             }
             return false;
         });
+    }
 
+    private void switchActivity(Class<?> activityClass) {
+        Intent intent = new Intent(getApplicationContext(), activityClass);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initializeFirebaseAndRecyclerView() {
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
         rvBlog = findViewById(R.id.rvBlog);
         blogItems = new ArrayList<>();
-        adapter = new BlogAdapter(this, blogItems);
+        filteredBlogItems = new ArrayList<>();
+        adapter = new BlogAdapter(this, filteredBlogItems);
         rvBlog.setAdapter(adapter);
         rvBlog.setLayoutManager(new LinearLayoutManager(this));
+        loadDataFromFirebase();
+    }
 
-        adapter.setOnItemClickListener(new BlogAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                BlogItem selectedBlog = blogItems.get(position);
-                Intent intent = new Intent(BlogCategoryActivity.this, BlogDetailActivity.class);
-                intent.putExtra("blogId", selectedBlog.getBlogId());
-                intent.putExtra("blogTitle", selectedBlog.getBlogTitle());
-                intent.putExtra("blogImage", selectedBlog.getBlogImage());
-                intent.putExtra("blogContent", selectedBlog.getBlogContent());
-                startActivity(intent);
-            }
-        });
+    private void setupSearchView() {
+        searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(this);
+    }
 
+    private void loadDataFromFirebase() {
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -90,6 +94,8 @@ public class BlogCategoryActivity extends AppCompatActivity {
                     BlogItem blog = snapshot.getValue(BlogItem.class);
                     blogItems.add(blog);
                 }
+                filteredBlogItems.clear();
+                filteredBlogItems.addAll(blogItems);
                 adapter.notifyDataSetChanged();
             }
 
@@ -98,5 +104,28 @@ public class BlogCategoryActivity extends AppCompatActivity {
                 Toast.makeText(BlogCategoryActivity.this, "Failed to load blogs.", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        filterBlogs(newText);
+        return true;
+    }
+
+    private void filterBlogs(String query) {
+        filteredBlogItems.clear();
+        if (query.isEmpty()) {
+            filteredBlogItems.addAll(blogItems);
+        } else {
+            filteredBlogItems.addAll(blogItems.stream()
+                    .filter(blog -> blog.getBlogTitle().toLowerCase().contains(query.toLowerCase()))
+                    .collect(Collectors.toList()));
+        }
+        adapter.notifyDataSetChanged();
     }
 }
